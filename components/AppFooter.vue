a260af1b2a9686901f0ed5eab5ac650fb65249f7
zeitchef <zeitchef@gmail.com>
2020-10-05 14:56:37 +0200

    Add mailto link in footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,9 +1,14 @@
 <template>
   <div id="footer" class="flex items-center justify-between w-screen h-12 p-8">
-    <p>{{ pageTitle }}</p>
+    <p v-if="pageTitle === 'zeitchef@gmail.com'">
+      <a class="footer-link" href="mailto:zeitchef@gmail.com">{{ pageTitle }}</a>
+    </p>
+    <p v-else>
+      {{ pageTitle }}
+    </p>
     <div id="social">
-      <a href="">github</a>
-      <a href="">twitter</a>
+      <a href="https://github.com/zeitchef" class="footer-link">github</a>
+      <a href="https://twitter.com/zeitchef" class="footer-link">twitter</a>
     </div>
   </div>
 </template>
@@ -23,8 +28,4 @@ export default {
 </script>
 
 <style scoped>
-a {
-  color: var(--black);
-  text-decoration: none;
-}
 </style>
