7a723915dc5be9453a9804ae7cdb2197631b9876
zeitchef <zeitchef@gmail.com>
2020-10-05 14:55:36 +0200

    Add Base16-snazzy colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,28 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
+  theme: {
+    extend: {
+      colors: {
+        'snazzy-gray-900': '#282a36',
+        'snazzy-gray-800': '#34353e',
+        'snazzy-gray-700': '#43454f',
+        'snazzy-gray-500': '#78787e',
+        'snazzy-gray-300': '#a5a5a9',
+        'snazzy-gray-100': '#e2e4e5',
+        'snazzy-white': '#f1f1f0',
+        'snazzy-red': '#ff5c57',
+        'snazzy-orange': '#ff9f43',
+        'snazzy-yellow': '#f3f99d',
+        'snazzy-green': '#5af78e',
+        'snazzy-blue': '#57c7ff',
+        'snazzy-pink': '#ff6ac1',
+      },
+    },
+  },
+  variants: {
+    backgroundColor: [ 'hover', 'focus', 'active' ],
+    opacity: [ 'hover', 'focus', 'active' ],
+    text: [ 'hover', 'focus', 'active' ],
+  }
 };
