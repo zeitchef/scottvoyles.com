1575767371c59b6a7e4e9e59cd8679b2f7aa2f35
zeitchef <zeitchef@gmail.com>
2020-10-06 15:38:54 +0200

    Add server configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,17 +8,24 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'The website of Scott Voyles, developer and musician' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'The website of Scott Voyles, developer and musician',
+      },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;500;600&display=swap',
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    '@/assets/css/globals'
-  ],
+  css: ['@/assets/css/globals', '@/assets/css/reset', '@/assets/css/base'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
@@ -33,11 +40,7 @@ export default {
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxt/content',
-    '@nuxtjs/robots'
-  ],
+  modules: ['@nuxtjs/axios', '@nuxt/content', '@nuxtjs/robots'],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
@@ -48,5 +51,11 @@ export default {
   robots: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {}
-}
+  build: {},
+
+  server: {
+    port: 8000,
+    host: '0.0.0.0',
+    timing: false,
+  },
+};
