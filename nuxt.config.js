7eb9799a71fd944914d4944869b46731e1bd8f65
zeitchef <zeitchef@gmail.com>
2020-10-06 23:51:09 +0200

    All html lang attr

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'Scott Voyles',
+    htmlAttrs: {
+      lang: 'en-US',
+    },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
