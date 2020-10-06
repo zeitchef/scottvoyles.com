fd0fa273b33acf3fddd0c791d9db49ed2fe2ad2d
zeitchef <zeitchef@gmail.com>
2020-10-06 15:36:45 +0200

    Add max-width to all pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <main class="max-w-6xl px-8 mx-auto mt-8">
     <div class="page__left">
-      <h1>Scott Voyles</h1>
+      <h1 class="mb-4">Scott Voyles</h1>
       <p>Amet totam quia nesciunt illum repudiandae laboriosam odio Error perferendis asperiores voluptatem debitis ex. Veritatis culpa odit molestiae qui saepe! Eligendi possimus est voluptatem omnis quibusdam Repudiandae minima id laboriosam excepturi sunt Earum cumque eligendi doloribus doloribus magni, adipisci tenetur, architecto Porro fugiat autem consequuntur repellendus corporis Commodi et fugiat?</p>
+
+      <p>If you're interested in my other life as a musician, click <a href="#">here</a>.</p>
     </div>
-    <div class="page__right">
-    </div>
-  </div>
+  </main>
 </template>
 
 <script>
@@ -18,4 +18,7 @@ export default{
 </script>
 
 <style scoped>
+p {
+  @apply mb-4
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="flex flex-col justify-center">
-    <h1 class="text-4xl">My name is Scott Voyles.</h1>
-    <h2 class="text-4xl">I'm a frontend software developer and musician.</h2>
-    <button class="w-32 p-2 mt-8 uppercase border border-black hover:font-bold transiton-colors duration-500 ease-in-out hover:bg-snazzy-gray-900 hover:text-snazzy-gray-100">Contact me</button>
-  </div>
+  <main class="flex flex-col justify-center px-8">
+    <section class="lg:pl-24">
+      <h1 class="text-2xl md:text-3xl">My name is Scott Voyles.</h1>
+      <h2 class="text-2xl md:text-3xl">I'm a frontend software developer and musician.</h2>
+      <button class="transiton-colors duration-500 ease-in-out hover:bg-snazzy-gray-900 hover:text-snazzy-gray-100">
+        Contact me
+      </button>
+    </section>
+  </main>
 </template>
 
 <script>
