ff13780595d535e23505f03a2cd099a24d49c938
zeitchef <zeitchef@gmail.com>
2020-10-06 23:56:08 +0200

    Remove heaviest Google Font weight

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       {
         rel: 'stylesheet',
         href:
-          'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;500;600&display=swap',
+          'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;500&display=swap',
       },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
     ],
