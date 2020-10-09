6e64bb95cc05f85c11dd4c48b9cea696c83eeeb2
zeitchef <zeitchef@gmail.com>
2020-10-09 11:08:02 +0200

    Update nav-links selectors

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="footer" class="flex items-center justify-center w-screen h-12 p-8 md:justify-between">
     <p v-if="$route.path == '/'" class="not-social">
-      <a class="footer-link" href="mailto:zeitchef@gmail.com">{{ pageTitle }}</a>
+      <a class="nav-link" href="mailto:zeitchef@gmail.com">{{ pageTitle }}</a>
     </p>
     <p v-else-if="$route.path == '/contact'" class="not-social">{{ pageTitle }}</p>
     <p v-else>{{ pageTitle }}</p>
 
-    <div v-if="$route.path == '/about'" class="invisible md:visible">02</div>
-    <div v-if="$route.path == '/tools'" class="invisible md:visible">03</div>
     <div id="social" v-if="$route.path == '/' || $route.path == '/contact'">
-      <a href="https://github.com/zeitchef" class="footer-link">github</a>
-      <a href="https://twitter.com/zeitchef" class="footer-link">twitter</a>
-      <a href="https://linkedin.com/in/zeitchef" class="footer-link">linkedin</a>
+      <a href="https://github.com/zeitchef" class="nav-link">github</a>
+      <a href="https://twitter.com/zeitchef" class="nav-link">twitter</a>
+      <a href="https://linkedin.com/in/zeitchef" class="nav-link">linkedin</a>
     </div>
   </div>
 </template>
@@ -39,7 +37,7 @@ export default {
 </script>
 
 <style scoped>
-#social .footer-link { @apply ml-4 }
+#social .nav-link { @apply ml-4 }
 
 @media (max-width: 768px) {
   .not-social {
