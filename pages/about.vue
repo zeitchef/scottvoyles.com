91156923fac59e08f97702f3505957598fd6d59e
zeitchef <zeitchef@gmail.com>
2020-10-10 17:22:31 +0200

    Refactor styles for pages/about

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,15 @@
 <template>
-  <main class="max-w-6xl px-8 mx-auto mt-8 mb-16 md:mb-0">
+  <main class="flex flex-col max-w-6xl px-8 mx-auto mt-8 mb-16 md:justify-center md:mb-0">
     <div class="page__left">
       <h1 class="mb-4">Scott Voyles</h1>
 
       <p>Since 2016, I've worked professionally as a frontend software developer in startup and large corporate settings, and as freelancer. My clients include global agencies all the way to individuals.</p>
+
+      <p>A list of languages, tools and gear I use everyday can be found <n-link to="/uses">here</n-link>.</p>
+
+      <div class="flex justify-center">
+        <hr class="w-3/5 mt-4 mb-8" />
+      </div>
 
       <p>Music has always been my life.</p>
 
