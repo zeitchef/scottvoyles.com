48749065e4f3dbbc957d6b9d6499fabbe36465cd
zeitchef <zeitchef@gmail.com>
2020-10-12 15:30:24 +0200

    Refactor anchors to nuxt-link components

--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- https://codepen.io/RSH87/pen/rmgYbo?editors=0100 -->
   <div class="p8">
-    <n-link to="/" class="nav-link" no-prefetch>home</n-link>
+    <n-link to="/" class="nav-link">home</n-link>
     <n-link to="/about" class="nav-link">about</n-link>
     <n-link to="/contact" class="nav-link">contact</n-link>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <p class="text-2xl font-medium md:text-3xl">My name is Scott Voyles.</p>
       <p class="text-2xl md:text-3xl">I'm a frontend software developer and musician.</p>
       <button class="transiton-colors duration-500 ease-in-out hover:bg-snazzy-gray-900 hover:text-snazzy-gray-100 dark:border-snazzy-white dark:hover:bg-snazzy-white dark:hover:text-snazzy-gray-800">
-        <a href="/contact" class="border-none">Contact me</a>
+        <nuxt-link to="/contact" class="border-none">Contact me</nuxt-link>
       </button>
     </section>
   </main>
