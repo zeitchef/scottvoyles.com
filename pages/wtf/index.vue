b26fc3a9ed031491e4bd6b4df01b1a94c3d28ddb
zeitchef <zeitchef@gmail.com>
2020-10-25 23:46:36 +0100

    List cheatsheets as tags

--- pages/wtf/index.vue
+++ pages/wtf/index.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="w-full max-w-6xl px-8 mx-auto mt-8 mb-16 md:mb-0">
     <h1 class="mb-4 text-3xl">WTF</h1>
-    <p>My cheatsheets collection</p>
-    <section class="mt-8">
-      <p v-for="cheatsheet in cheatsheets" :key="cheatsheet.slug">
-        <n-link :to="{ name: 'wtf-slug', params: { slug: cheatsheet.slug } }">
-          {{ cheatsheet.title }}
-        </n-link>
-      </p>
-    </section>
+    <p class="mb-8">My cheatsheets collection</p>
+    <main>
+      <section class="flex flex-wrap justify-between mt-8">
+        <article v-for="cheatsheet in cheatsheets"
+                 :key="cheatsheet.slug"
+                 class="p-2 mr-4 text-white rounded bg-snazzy-gray-500">
+          <n-link :to="{ name: 'wtf-slug', params: { slug: cheatsheet.slug } }">
+            {{ cheatsheet.title }}
+          </n-link>
+        </article>
+      </section>
+    </main>
   </div>
 </template>
 
@@ -26,7 +30,15 @@
 </script>
 
 <style scoped>
-h4 a {
-  @apply no-underline
+main > :not(:last-child) {
+  @apply mb-8
+}
+
+section {
+  margin-top: -1rem;
+}
+
+section > * {
+  margin-top: 1rem;
 }
 </style>
