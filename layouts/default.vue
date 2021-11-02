f7d968a15819e69bdc04121241220f63efb678f4
zeitchef <zeitchef@gmail.com>
2021-11-02 20:43:46 +0100

    feat: update dark mode gradient color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex flex-col min-h-screen bg-white text-snazzy-gray-800 dark:bg-snazzy-gray-800 dark:text-snazzy-white gradient-light dark:gradient-dark">
+  <div class="flex flex-col min-h-screen bg-white text-snazzy-gray-800 dark:bg-snazzy-gray-800 dark:text-snazzy-white gradient-light dark:gradient-dark" >
     <AppHeader />
     <Nuxt class="content" />
-    <AppFooter class="flex-shrink-1"/>
+    <AppFooter class="flex-shrink-1" />
   </div>
 </template>
 
 <style>
-.content {flex: 1 0 auto; }
+.content {
+  flex: 1 0 auto;
+}
 </style>
