962091c754d99d54be8673f0f1d33079919e9f54
zeitchef <zeitchef@gmail.com>
2021-11-02 21:07:25 +0100

    fix formatting

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,14 @@
 <template>
   <main class="flex flex-col justify-center px-8">
     <section class="lg:pl-24">
-      <p class="mb-3 text-2xl text-snazzy-gray-500 dark:text-snazzy-gray-300">{{ hello }}</p>
+      <p class="mb-3 text-2xl text-snazzy-gray-500 dark:text-snazzy-gray-300">
+        {{ hello }}
+      </p>
       <p class="text-2xl font-medium md:text-3xl">My name is Scott Voyles.</p>
-      <p class="text-2xl md:text-3xl">I'm a frontend software developer and musician.</p>
-      <button class="transiton-colors duration-500 ease-in-out hover:bg-snazzy-gray-900 hover:text-snazzy-gray-100 dark:border-snazzy-white dark:hover:bg-snazzy-white dark:hover:text-snazzy-gray-800">
+      <p class="text-2xl md:text-3xl">
+        I'm a frontend software developer and musician.
+      </p>
+      <button class="transiton-colors duration-500 ease-in-out hover:bg-snazzy-gray-900 hover:text-snazzy-gray-100 dark:border-snazzy-white dark:hover:bg-snazzy-white dark:hover:text-snazzy-gray-800" >
         <nuxt-link to="/contact" class="border-none">Contact me</nuxt-link>
       </button>
     </section>
@@ -15,8 +19,8 @@
 export default {
   data() {
     return {
-      hello: `<Hello />`
+      hello: `<Hello />`,
     }
-  }
+  },
 }
 </script>
