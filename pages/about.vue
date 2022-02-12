7d305a44155d58d413dd512db328ca50cbac242a
Scott Voyles <zeitchef@gmail.com>
2022-02-12 22:51:57 +0100

    fix(html): more semantic HTML

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,39 @@
 <template>
-  <main class="flex flex-col max-w-6xl px-8 mx-auto mt-8 mb-16 md:justify-center md:mb-0">
-    <div class="page__left">
+  <main
+    class="flex flex-col max-w-6xl px-8 mx-auto mt-8 mb-16 md:justify-center md:mb-0"
+  >
+    <article>
       <h1 class="mb-4">Scott Voyles</h1>
 
-      <p>Since 2016, I've worked professionally as a frontend software developer in startup and large corporate settings, and as freelancer. My clients include global agencies all the way to individuals.</p>
+      <section>
+        <p class="mb-4">
+          Since 2016, I've worked professionally as a frontend software
+          developer in startup and large corporate settings, and as freelancer.
+          My clients include global agencies all the way to individuals.
+        </p>
 
-      <!-- <p>A list of languages, tools and gear I use everyday can be found <n-link to="/uses">here</n-link>.</p> -->
+        <!-- <p>A list of languages, tools and gear I use everyday can be found <n-link to="/uses">here</n-link>.</p> -->
+      </section>
 
       <div class="flex justify-center">
         <hr class="w-3/5 mt-4 mb-8" />
       </div>
 
-      <p>Music has always been my life.</p>
+      <section>
+        <p class="mb-4">Music has always been my life.</p>
 
-      <p>I have three degress in music performance from the University of Louisville School of Music, the Indiana University Jacobs School of Music, and the Hochschule für Musik und Darstellende Kunst in Frankfurt am Main, Germany.</p>
+        <p class="mb-4">
+          I have three degress in music performance from the University of
+          Louisville School of Music, the Indiana University Jacobs School of
+          Music, and the Hochschule für Musik und Darstellende Kunst in
+          Frankfurt am Main, Germany.
+        </p>
 
-      <p>If you're interested in my other life as a musician, click <a href="https://en.wikipedia.org/wiki/Scott_Voyles">here</a>.</p>
-    </div>
+        <p class="mb-4">
+          If you're interested in my other life as a musician, click
+          <a href="https://en.wikipedia.org/wiki/Scott_Voyles">here</a>.
+        </p>
+      </section>
+    </article>
   </main>
 </template>
-
-<style scoped>
-p {
-  @apply mb-4
-}
-</style>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,31 +1,49 @@
 <template>
   <client-only>
     <main class="flex items-center max-w-6xl px-8 mx-auto mt-8">
-
       <section class="flex-1 p-8 mb-16 md:mb-0 contact contact-left">
-        <form name="contact" method="post" data-netlify-honeypot="bot-field" data-netlify="true">
+        <form
+          name="contact"
+          method="post"
+          data-netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
           <input type="hidden" value="contact" name="form-name" />
           <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" id="name" name="name" class="block w-full">
+            <input type="text" id="name" name="name" class="block w-full" />
           </div>
           <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" id="email" name="email" class="block w-full">
+            <input type="email" id="email" name="email" class="block w-full" />
           </div>
           <div class="form-group">
             <label for="message">Message</label>
-            <textarea name="message" id="message" cols="30" rows="10" class=""></textarea>
+            <textarea
+              name="message"
+              id="message"
+              cols="30"
+              rows="10"
+              class=""
+            ></textarea>
           </div>
-          <button type="submit" class="transiton-colors duration-500 ease-in-out hover:bg-snazzy-gray-900 hover:text-snazzy-gray-100 dark:border-snazzy-white dark:hover:text-snazzy-gray-800 dark:hover:bg-snazzy-white">
+          <button
+            type="submit"
+            class="transiton-colors duration-500 ease-in-out hover:bg-snazzy-gray-900 hover:text-snazzy-gray-100 dark:border-snazzy-white dark:hover:text-snazzy-gray-800 dark:hover:bg-snazzy-white"
+          >
             Send
           </button>
         </form>
       </section>
 
-      <section class="flex flex-col items-start flex-1 p-8 mb-16 contact contact-right">
+      <section
+        class="flex flex-col items-start flex-1 p-8 mb-16 contact contact-right"
+      >
         <h1 class="mb-4 font-medium">Thanks for stopping by</h1>
-        <p class="mb-4">If you like my work, please feel free to write to me. I look forward to seeing what we can do together.</p>
+        <p class="mb-4">
+          If you like my work, please feel free to write to me. I look forward
+          to seeing what we can do together.
+        </p>
       </section>
     </main>
   </client-only>
@@ -36,21 +54,28 @@
   main {
     flex-direction: column;
   }
-  section { flex: 0; }
+  section {
+    flex: 0;
+  }
 
   .contact {
     padding: 0;
     min-width: 100%;
   }
-  .contact-left { order: 2; }
-  .contact-right { order: 1; }
+  .contact-left {
+    order: 2;
+  }
+  .contact-right {
+    order: 1;
+  }
 }
 
-input, textarea {
-  @apply block w-full
+input,
+textarea {
+  @apply block w-full;
 }
 
 .form-group {
-  @apply border-b border-snazzy-gray-300 mb-4
+  @apply border-b border-snazzy-gray-300 mb-4;
 }
 </style>
